281a9559c38f712d7c661102c13241312284d1a0
Edson Barros Ponciúncula <barrosedsonp@gmail.com>
2025-12-11 15:38:23 -0300

    Correção models.py com o algoritmo .\cria_usuario_admin.py

--- core/sistemachamado.py
+++ core/sistemachamado.py
@@ -2,7 +2,7 @@ from models.models import extrair_dados, salvar_dados
 
 class SistemaChamado:
     def __init__(self):
-        self.chamados = extrair_dados()
+        self.chamados = extrair_dados('registro_chamados')
         
     def cadastrar_chamado(self, chamado):
         """
@@ -23,7 +23,7 @@ class SistemaChamado:
             "status": chamado.status
         }
         
-        salvar_dados(self.chamados)
+        salvar_dados(self.chamados, 'registro_chamados')
     
     def consultar_chamados(self):
         return self.chamados.items()
@@ -31,21 +31,21 @@ class SistemaChamado:
     def alterar_status(self, codigo, status):
         if (codigo in self.chamados) and (status.upper() in ['ABERTO', 'FECHADO', 'EM ANDAMENTO', 'ANDAMENTO']):
             self.chamados[codigo]['status'] = status.title()
-            salvar_dados(self.chamados)
+            salvar_dados(self.chamados, 'registro_chamados')
             return True
         return False
     
     def excluir_chamado(self, codigo):
         if codigo in self.chamados:
             self.chamados.pop(codigo)
-            salvar_dados(self.chamados)
+            salvar_dados(self.chamados, 'registro_chamados')
             return True
         return False
     
     def editar_chamado(self, codigo, campo, novo_valor):
         if (codigo in self.chamados) and (campo.upper() in ['NOME_SOLICITANTE', 'SETOR', 'TIPO_SERVICO', 'LOCALIZACAO', 'DESCRICAO', 'PRIORIDADE']):
             self.chamados[codigo][f'{campo.lower()}'] = novo_valor
-            salvar_dados(self.chamados)
+            salvar_dados(self.chamados, 'registro_chamados')
             return True
         return False
     
--- cria_usuario_admin.py
+++ cria_usuario_admin.py
@@ -2,6 +2,7 @@
 Algoritmo para criação dos usuários administradores do sistema
 """
 from werkzeug.security import generate_password_hash
+from models.models import extrair_dados, salvar_dados
 import json
 import secrets
 import os
@@ -10,19 +11,6 @@ def verifica_arquivo():
     if not(os.path.isfile('models/usuarios_admin.json')):
         with open('models/usuarios_admin.json', 'w') as arquivo:
             json.dump({}, arquivo)
-
-def extrair_dados():
-    with open('models/usuarios_admin.json', 'r') as arquivo:
-        dados = json.load(arquivo)
-    return dados
-
-def salvar_dados(dados_atualizados):
-    try:
-        with open('models/usuarios_admin.json', 'w') as arquivo:
-            json.dump(dados_atualizados, arquivo, ensure_ascii=True, indent=4)
-        return 'Usuário cadastrado com sucesso!'
-    except Exception as erro:
-        return f'Não foi possível cadastrar o usuário: {erro}'
     
 def verifica_email_cadastrado(dados, email):
     for dado in dados.values():
@@ -31,7 +19,8 @@ def verifica_email_cadastrado(dados, email):
     return False
 
 def main():
-    dados = extrair_dados()
+    verifica_arquivo()
+    dados = extrair_dados('usuarios_admin')
     nome_completo = str(input('Nome do usuário: ')).strip()
     celular = str(input('Celular: ')).strip()
     email = str(input('E-mail do usuário: ')).strip()
@@ -42,14 +31,13 @@ def main():
     
     senha_pura = str(input('Senha: ')).strip()
     id = secrets.token_hex(8)
-    verifica_arquivo()
     dados[id] = {
         "nome": nome_completo,
         "celular": celular,
         "email": email,
         "senha": generate_password_hash(senha_pura)
     }
-    print(salvar_dados(dados))
+    salvar_dados(dados, 'usuarios_admin')
     
 if __name__ == '__main__':
     main()
